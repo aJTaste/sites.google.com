01e0f000b77ad13e9835fda7c517dc1e8611970c
Taste <4hrji46478m@gmail.com>
2025-12-17 00:51:12 +0000

    uv一旦消す

--- public/sites.google.com/uv/app.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById('url');
-const iframe = document.getElementById('view');
-const btn = document.getElementById('go');
-
-btn.onclick = () => {
-  const url = encodeURIComponent(input.value);
-  iframe.src = `/proxy?url=${url}`;
-};
