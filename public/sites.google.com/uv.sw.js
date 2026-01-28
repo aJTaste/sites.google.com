4cc510e38c2c530bd7908555cd78e612e05f3a61
Taste <ajtaste-001@outlook.jp>
2026-01-28 23:01:55 +0900

    Update uv.sw.js

--- public/sites.google.com/uv.sw.js
+++ public/sites.google.com/uv.sw.js
@@ -5,6 +5,14 @@ importScripts('/sites.google.com/js/uv.handler.js');
 
 const uv=new UVServiceWorker();
 
+self.addEventListener('install',()=>{
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch',event=>{
   event.respondWith(
     uv.fetch(event).catch(()=>fetch(event.request))
