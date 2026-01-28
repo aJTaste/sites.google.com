cbc9bfee29eaf75980ae3b91351c9feddc886237
Taste <ajtaste-001@outlook.jp>
2026-01-28 23:13:37 +0900

    Update uv.config.js

--- public/sites.google.com/uv.config.js
+++ public/sites.google.com/uv.config.js
@@ -1,11 +1,20 @@
-// public/sites.google.com/uv.config.js
 self.__uv$config={
   prefix:'/sites.google.com/service/',
   bare:'https://uv.holy.how/bare/',
-  encodeUrl:Ultraviolet.codec.xor.encode,
-  decodeUrl:Ultraviolet.codec.xor.decode,
-  handler:'/sites.google.com/js/uv.handler.js',
-  bundle:'/sites.google.com/js/uv.bundle.js',
+  encodeUrl:url=>{
+    if(typeof Ultraviolet==='undefined'){
+      return btoa(url);
+    }
+    return Ultraviolet.codec.xor.encode(url);
+  },
+  decodeUrl:encoded=>{
+    if(typeof Ultraviolet==='undefined'){
+      return atob(encoded);
+    }
+    return Ultraviolet.codec.xor.decode(encoded);
+  },
+  handler:'/sites.google.com/uv.handler.js',
+  bundle:'/sites.google.com/uv.bundle.js',
   config:'/sites.google.com/uv.config.js',
   sw:'/sites.google.com/uv.sw.js'
 };
