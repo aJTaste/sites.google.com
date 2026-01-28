2f1f6a60f13851e966141a395a1a53892d3f835d
Taste <ajtaste-001@outlook.jp>
2026-01-28 17:58:06 +0900

    Create uv.config.js

---
+++ public/sites.google.com/uv.config.js
@@ -0,0 +1,11 @@
+// public/sites.google.com/uv.config.js
+self.__uv$config={
+  prefix:'/sites.google.com/service/',
+  bare:'https://uv.holy.how/bare/',
+  encodeUrl:Ultraviolet.codec.xor.encode,
+  decodeUrl:Ultraviolet.codec.xor.decode,
+  handler:'/sites.google.com/uv.bundle.js',
+  bundle:'/sites.google.com/uv.bundle.js',
+  config:'/sites.google.com/uv.config.js',
+  sw:'/sites.google.com/uv.sw.js'
+};
