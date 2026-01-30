da4bc539df238d53beed3c839a0b80380e2a7e30
Taste <ajtaste-001@outlook.jp>
2026-01-30 10:58:09 +0900

    Update config and sw paths in uv.config.js

--- public/sites.google.com/uv/uv.config.js
+++ public/sites.google.com/uv/uv.config.js
@@ -10,8 +10,8 @@ self.__uv$config={
   handler:'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.handler.js',
   client:'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.client.js',
   bundle:'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.7/dist/uv.bundle.js',
-  config:'/sites.google.com/uv/uv.config.js',
-  sw:'/sites.google.com/uv/uv.sw.js'
+  config:'/public/sites.google.com/uv/uv.config.js',
+  sw:'/public/sites.google.com/uv/uv.sw.js'
 };
 
 console.log('✅ [UV-CONFIG] 設定完了',self.__uv$config);
