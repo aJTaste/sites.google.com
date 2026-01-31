ef15e36a224e784521e899bc77c88a4b793f1848
Taste <ajtaste-001@outlook.jp>
2026-01-31 16:02:29 +0900

    Create uv.config.js

---
+++ public/sites.google.com/uv/uv.config.js
@@ -0,0 +1,11 @@
+// Ultraviolet設定
+self.__uv$config={
+  prefix:'/sites.google.com/uv/service/',
+  bare:'/bare/',
+  encodeUrl:Ultraviolet.codec.xor.encode,
+  decodeUrl:Ultraviolet.codec.xor.decode,
+  handler:'/sites.google.com/uv/uv.handler.js',
+  bundle:'/sites.google.com/uv/uv.bundle.js',
+  config:'/sites.google.com/uv/uv.config.js',
+  sw:'/sites.google.com/uv/uv.sw.js'
+};
