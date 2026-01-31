a867000d1316f945cec7cb2e1615a5f570aa291f
Taste <ajtaste-001@outlook.jp>
2026-01-31 21:15:27 +0900

    Create scramjet.sw.js

---
+++ public/sites.google.com/scramjet/scramjet.sw.js
@@ -0,0 +1,4 @@
+importScripts('https://unpkg.com/@mercuryworkshop/scramjet/dist/scramjet.codecs.js');
+importScripts('/sites.google.com/scramjet/scramjet.config.js');
+importScripts('https://unpkg.com/@mercuryworkshop/scramjet/dist/scramjet.sw.js');
+console.log('[Scramjet SW] Service Worker起動完了');
