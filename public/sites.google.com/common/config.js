0ce323b72089730a3bda2a3c3ee08fa7449d9823
Taste <ajtaste-001@outlook.jp>
2026-02-09 12:59:33 +0000

    a

--- public/sites.google.com/common/config.js
+++ public/sites.google.com/common/config.js
@@ -3,8 +3,8 @@
 // ナビゲーションアイテム
 export const NAV_ITEMS=[
   {id:'hub',icon:'hub',title:'Hub',href:'/sites.google.com/hub.html'},
-  {id:'chat',icon:'chat',title:'ChatHub',href:'/sites.google.com/chat.html'},
-  {id:'gate',icon:'partner_exchange',title:'aJTGate',href:'/sites.google.com/gate.html'},
+  {id:'chat',icon:'forum',title:'ChatHub',href:'/sites.google.com/chat.html'},
+  {id:'gate',icon:'disabled_by_default',title:'aJTGate',href:'/sites.google.com/gate.html'},
   {id:'games',icon:'stadia_controller',title:'Games',href:'/sites.google.com/games.html'},
   {id:'proxy',icon:'vpn_key',title:'Proxy',href:'/sites.google.com/proxy.html'},
   {id:'docs',icon:'edit_note',title:'Docs',href:'/sites.google.com/docs.html'},
