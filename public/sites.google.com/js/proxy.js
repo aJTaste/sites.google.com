27667edaf7ef571ceab5e2b43433243ebe640ac3
Taste <ajtaste-001@outlook.jp>
2026-02-28 22:25:25 +0900

    Update proxy.js

--- public/sites.google.com/js/proxy.js
+++ public/sites.google.com/js/proxy.js
@@ -10,7 +10,13 @@ const PROXY_BASE='https://corsproxy.io/?url=';
 const TIMEOUT_MS=15000;
 
 function corsProxyUrl(url){
-  return PROXY_BASE+url;
+  // スキームとホスト部分を壊さずにそれ以外だけエンコード
+  try{
+    const u=new URL(url);
+    return PROXY_BASE+u.href;
+  }catch(e){
+    return PROXY_BASE+url;
+  }
 }
 
 // ========================================
